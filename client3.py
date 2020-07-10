579dc85786ba2219acd8959d75f91c3b54b88926
Ronen Rokhit <39106199+rrokhit@users.noreply.github.com>
2020-07-10 08:09:28 -0400

    returns correct price and ratio

--- client3.py
+++ client3.py
@@ -31,18 +31,19 @@ N = 500
 
 def getDataPoint(quote):
 	""" Produce all of the needed values to generate a datapoint """
-	""" ------------- Update this function ------------- """
 	stock = quote['stock']
 	bid_price = float(quote['top_bid']['price'])
 	ask_price = float(quote['top_ask']['price'])
-	price = bid_price
+	price = (bid_price + ask_price)/2
 	return stock, bid_price, ask_price, price
 
 def getRatio(price_a, price_b):
 	""" Get ratio of price_a and price_b """
-	""" ------------- Update this function ------------- """
+	if price_b == 0:
+		raise Exception("Price B is 0, cannot produce ratio")
+	else:
+		return price_a/price_b
 	""" Also create some unit tests for this function in client_test.py """
-	return 1
 
 # Main
 if __name__ == "__main__":
